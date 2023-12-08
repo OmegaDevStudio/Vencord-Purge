f0676c78d785087e1b35b82525fe7e79191e10a9
Shell1010 <amin.dev03@gmail.com>
2023-12-08 19:19:49 +0000

    Initial

---
+++ messagePurge.ts
@@ -0,0 +1,91 @@
+
+import { findByPropsLazy } from "@webpack";
+import definePlugin, { OptionType } from "@utils/types";
+import { ApplicationCommandInputType, ApplicationCommandOptionType, ApplicationCommandType, findOption, registerCommand, sendBotMessage, unregisterCommand } from "@api/Commands";
+import { MessageStore, UserStore } from "@webpack/common";
+import { Message, Channel } from "discord-types/general";
+import { MessageActions } from "@utils/discord";
+const MessageDelete = findByPropsLazy("deleteMessage", "patchMessageAttachments");
+
+
+
+function DeleteMessages(amount: number, channel: Channel) {
+    const meId = UserStore.getCurrentUser().id;
+    const messages: Message[] = MessageStore.getMessages(channel.id)._array.filter(m => m.author.id === meId).reverse().slice(0, amount);
+    var msgs: Message[] = JSON.parse(JSON.stringify(messages));
+    var counter = 0;
+    msgs.forEach(msg=>{ MessageActions.deleteMessage(channel.id, msg.id); counter += 1});
+    return counter
+}
+
+
+
+
+export default definePlugin({
+    name: "MessagePurge",
+    description: "Purges messages from a channel",
+    dependencies: ["CommandsAPI"],
+    authors: [{name: "Shell", id: 1056383259325513888n}],
+    options: {
+        baddies: {
+            name: "I will be a baddie",
+            description: "If enabled I will become baddie",
+            type: OptionType.BOOLEAN,
+            default: true
+        }
+    },
+
+    async start() {},
+    commands: [
+        {
+            name: "purge",
+            description: "Manage Purge related Menu",
+            inputType: ApplicationCommandInputType.BUILT_IN,
+            options: [
+                {
+                    name: "purge",
+                    description: "Begins purging messages by a set amount",
+                    type: ApplicationCommandOptionType.SUB_COMMAND,
+                    options: [
+                        {
+                            name: "amount",
+                            description: "How many messages you wish to purge",
+                            type: ApplicationCommandOptionType.INTEGER,
+                            required: true
+                        },
+                        {
+                            name: "channel",
+                            description: "Channel ID you wish to purge from",
+                            type: ApplicationCommandOptionType.CHANNEL,
+                            required: false
+                        }
+                    ]
+                }
+            ],
+
+            async execute(args, ctx) {
+                switch (args[0].name) {
+                    case "purge": {
+                        const amount: number = findOption(args[0].options, "amount", 0);
+                        const channel: Channel = findOption(args[0].options, "channel", ctx.channel);
+                        const len = DeleteMessages(amount, channel);
+                        return sendBotMessage(ctx.channel.id, {
+                            content: `Successfully deleted ${len} Messages`
+                        })
+
+
+
+                    }
+
+                    default: {
+                        return sendBotMessage(ctx.channel.id, {
+                            content: "Invalid sub-command"
+                        });
+                        
+                    }
+                }
+            }
+        }
+    ],
+    async end() {},
+})
